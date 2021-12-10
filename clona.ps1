a317cfd4c3c29b5bfab382d7d461ace68fbaba1e
Giorgio Lasala <salem84@users.noreply.github.com>
2021-12-10 20:09:51 +0100

    cmd to delete history

--- clona.ps1
+++ clona.ps1
@@ -5,3 +5,7 @@ cd $name
 git clone $reposurl .
 gh repo create salem84-eng/$name --private --source=. --push --remote=upstream
 cd ..
+
+
+# Per rimuovere un file dall'history
+# git filter-branch --tree-filter 'rm -rf Reports/Test DATASET/Test.pbix' HEAD
